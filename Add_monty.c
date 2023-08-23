c9d1e53dbee37b64876dff78eab26d119843b9e0
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-08-23 20:50:27 +0300

    deleteing a .out and adding a new line at the error message

--- Add_monty.c
+++ Add_monty.c
@@ -7,7 +7,7 @@ void monty_add(stack_t **head, unsigned int line_number)
 
 	if (!*head || !(*head)->next)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		fclose(monty.file);
 		free(monty.Getline);
 		free_stack_t(*head);
