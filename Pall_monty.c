42a2a5da47328e810a38491554fa5387edeb48f7
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-23 18:02:56 +0300

    update

--- Pall_monty.c
+++ Pall_monty.c
@@ -4,6 +4,11 @@ void monty_pall(stack_t **stack, unsigned int l __attribute__((unused)))
 {
 	stack_t *current = *stack;
 
+	if (current == NULL)
+	{
+		return;
+	}
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
