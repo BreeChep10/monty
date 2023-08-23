4d687455a3402d352d1bfa1b95935d4536c0660b
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-23 17:53:55 +0300

    update

--- Push_monty.c
+++ Push_monty.c
@@ -1,5 +1,5 @@
 #include "monty.h"
-
+/*
 void monty_push(stack_t **stack, unsigned int line_number)
 {
 	int value;
@@ -37,4 +37,36 @@ void monty_push(stack_t **stack, unsigned int line_number)
 	value = atoi(monty.second);
 	*stack = add_dnodeint(stack, value);
 
+}
+*/
+
+void monty_push(stack_t **head, unsigned int line_number)
+{
+	int n, j = 0, flag = 0;
+
+	if (monty.second)
+	{
+		if (monty.second[0] == '-')
+			j++;
+		for (; monty.second[j] != '\0'; j++)
+		{
+			if (monty.second[j] > 57 || monty.second[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			fclose(monty.file);
+			free(monty.Getline);
+			free_stack_t(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		fclose(monty.file);
+		free(monty.Getline);
+		free_stack_t(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(monty.second);
+	if (monty.mode == 0)
+		add_dnodeint(head, n);
+	else
+		add_dnodeint_end(head, n);
 }
