3f00fc8c08290fa10e8eff64c89169c6fd465828
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-08-24 10:49:18 +0300

    Mul opcode implementation

--- Mul_monty.c
+++ Mul_monty.c
@@ -1,32 +1,27 @@
 #include "monty.h"
-/**
-*  * f_mul - multiplies the top two elements of the stack.
-*   * @head: stack head
-*    * @counter: line_number
-*     * Return: no return
-*     */
-void monty_mul(stack_t **head, unsigned int counter)
-{
-stack_t *h;
-int len = 0, aux;
 
-h = *head;
-while (h)
+/**
+ * monty_mul - multiplies the top two elements of the stack.
+ * @head: The head of the stack.
+ * @counter: line_number position.
+ * Return: void.
+ */
+
+void monty_mul(stack_t **head, unsigned int line_number)
 {
-h = h->next;
-len++;
-}
-if (len < 2)
-{
-fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
-fclose(monty.file);
-free(monty.Getline);
-free_stack_t(*head);
-exit(EXIT_FAILURE);
-}
-h = *head;
-aux = h->next->n * h->n;
-h->next->n = aux;
-*head = h->next;
-free(h);
+	stack_t *current = *head;
+	int mul;
+
+	if (!*head || !(*head)->next)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		fclose(monty.file);
+		free(monty.Getline);
+		free_stack_t(*head);
+		exit(EXIT_FAILURE);
+	}
+	mul = current->next->n * current->n;
+	current->next->n = mul;
+	*head = current->next;
+	free(current);
 }
