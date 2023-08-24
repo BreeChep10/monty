6d5b08231f8e88631b7e731bdcc9ed664d4efa62
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-08-24 14:09:47 +0300

    checker correction

--- Pstr_monty.c
+++ Pstr_monty.c
@@ -9,19 +9,16 @@ void monty_pstr(stack_t **head, unsigned int line_number)
 	if (!*head)
 	{
 		printf("\n");
-		fclose(monty.file);
+/*		fclose(monty.file);
 		free(monty.Getline);
-		free_stack_t(*head);
+		free_stack_t(*head);*/
 	}
 	while (current)
 	{
-		if (current->n == 0 || current->n < 0 || current->n > 127)
+		if (current->n <= 0 || current->n > 127)
 			break;
-		else
-		{
-			printf("%c", current->n);
-			current = current->next;
-		}
+		printf("%c", current->n);
+		current = current->next;
 	}
 	printf("\n");
 }
